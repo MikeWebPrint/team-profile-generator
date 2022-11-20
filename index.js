8c9e21a5058bbf92fbb272372350232d4be3e816
Mike Johnson <mike@mapleprintservices.com>
2022-11-20 14:18:13 -0500

    employee classes extending successfully

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const teamRoles = require('./lib/teamRoles');
+const employee = require('./lib/employee');
 
-const Employee = new teamRoles.Employee;
-const Manager = new teamRoles.Manager;
-const Engineer = new teamRoles.Engineer;
-const Intern = new teamRoles.Intern;
+
+const Manager =  employee.Manager;
+const Engineer = employee.Engineer;
+const Intern = employee.Intern;
+
+const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
+console.log(Mike);
+const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
+console.log(Jackie);
+const Joe = new Engineer('Joe', 36, 'joe@test.com', 'bigJoeD');
+console.log(Joe);
+console.log(Jackie.getSchool());
+console.log(Mike.getEmail());
+console.log(Joe.getGithub());
+console.log(Jackie.getEmail());
