e35aade7f9193d49cafd98ae87484b9a85436cd6
Mike Johnson <mike@mapleprintservices.com>
2022-11-20 16:07:14 -0500

    layout file template started and question types built

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const employee = require('./lib/employee');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions');
+
 
 
 const Manager =  employee.Manager;
 const Engineer = employee.Engineer;
 const Intern = employee.Intern;
 
-const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
-console.log(Mike);
-const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
-console.log(Jackie);
-const Joe = new Engineer('Joe', 36, 'joe@test.com', 'bigJoeD');
-console.log(Joe);
-console.log(Jackie.getSchool());
-console.log(Mike.getEmail());
-console.log(Joe.getGithub());
-console.log(Jackie.getEmail());
+// const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
+// const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
+// const Joe = new Engineer('Joe', 36, 'joe@test.com', 'bigJoeD');
+// console.log(Mike);
+// console.log(Joe);
+// console.log(Jackie);
+
+inquirer
+  .prompt(questions.managerQuest)
+  .then(questions.addMore)
+
+  // .prompt(questions.teamQuest)
+  // .prompt(questions.engineerQuest)
+  // .prompt(questions.internQest)
+  .then((response) =>{
+    console.log(response)
+  })
