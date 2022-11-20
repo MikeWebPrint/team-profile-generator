6211f544da9279c1a66f22d704888824992a8d0b
Mike Johnson <mike@mapleprintservices.com>
2022-11-20 13:10:15 -0500

    init classes

---
+++ index.js
@@ -0,0 +1,6 @@
+const teamRoles = require('./lib/teamRoles');
+
+const Employee = new teamRoles.Employee;
+const Manager = new teamRoles.Manager;
+const Engineer = new teamRoles.Engineer;
+const Intern = new teamRoles.Intern;
