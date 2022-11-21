6c78795373d9241a0f20412cea3a98f48a601432
Mike Johnson <mike@mapleprintservices.com>
2022-11-20 22:49:22 -0500

    simplified questions and inquirer method, writing data to json

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const employee = require('./lib/employee');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions');
-
-
-
-const Manager =  employee.Manager;
+const employee = require('./lib/employee');
+const Manager = employee.Manager;
 const Engineer = employee.Engineer;
 const Intern = employee.Intern;
+const team = [];
 
 // const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
 // const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
@@ -15,14 +13,28 @@ const Intern = employee.Intern;
 // console.log(Mike);
 // console.log(Joe);
 // console.log(Jackie);
+function createTeam() {
+  return inquirer.prompt(questions.teamQuest)
+    .then((answers) => {
+      team.push(answers)
+      if (answers.addAnotherMember) {
+        return createTeam()
+      } else {
+        console.log(team)
+        return (team)
+      }
+    }
+    )
+    .then((team) => {
+      fs.appendFile('./src/team.json', `${JSON.stringify(team, null, '\t')}\n`, (err) => err ? console.error(err) : console.log('Team member created!'))
+    })
+  }
 
-inquirer
-  .prompt(questions.managerQuest)
-  .then(questions.addMore)
-
-  // .prompt(questions.teamQuest)
-  // .prompt(questions.engineerQuest)
-  // .prompt(questions.internQest)
-  .then((response) =>{
-    console.log(response)
-  })
+createTeam()
+// parseTeam = (team) => {
+//   const teamList = {}
+//   team.forEach(member => {
+//     teamList.add(member);
+//   })
+//   console.log(teamList)
+// }
