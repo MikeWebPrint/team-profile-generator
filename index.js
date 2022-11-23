a06792ffb2b6940dc833e0c6cd8e5fbcde27f335
Mike Johnson <mike@mapleprintservices.com>
2022-11-22 21:48:19 -0500

    separated class files

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions');
-const employee = require('./lib/employee');
-const Manager = employee.Manager;
-const Engineer = employee.Engineer;
-const Intern = employee.Intern;
+// const employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 const team = [];
 
-// const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
-// const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
-// const Joe = new Engineer('Joe', 36, 'joe@test.com', 'bigJoeD');
-// console.log(Mike);
-// console.log(Joe);
-// console.log(Jackie);
+const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
+const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
+const Bob = new Intern('Bob', 456, 'bob@test.com', 'uconn')
+
+const Joe = new Engineer('Joe', 36, 'joe@test.com', 'bigJoeD');
+console.log(Mike);
+console.log(Joe);
+console.log(Jackie);
+console.log(Bob);
 function createTeam() {
   return inquirer.prompt(questions.teamQuest)
     .then((answers) => {
@@ -21,6 +24,7 @@ function createTeam() {
         return createTeam()
       } else {
         console.log(team)
+        console.log(answers)
         return (team)
       }
     }
