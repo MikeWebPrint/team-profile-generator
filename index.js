bf84397233efa9ff5428c3cc87b8f0f1a4c20a7d
Mike Johnson <mike@mapleprintservices.com>
2022-11-22 23:29:40 -0500

    progress on employee obj

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const addMember = require('./lib/addMember');
-const teamQuest = require('./lib/teamQuest');
+const empQuest = require('./lib/empQuest');
 
 // const employee = require('./lib/employee');
 const Manager = require('./lib/manager');
@@ -21,48 +21,44 @@ const team = [];
 
 
 function inquireMember() {
-  return inquirer.prompt(addMember)
+  inquirer.prompt(addMember)
     .then((answers) => {
       if (answers.addMember === 'Yes') {
         createMember();
       } else {
-        console.log('Done!')
-        return
+        createTeam();
       }
     })
 }
 
 function createMember() {
-  inquirer.prompt(teamQuest)
+  inquirer.prompt(empQuest)
     .then((answers) => {
       let { empName, role, empId, empEmail, github, internSchool, mgrOffice, } = answers;
       switch (role) {
         case 'Manager':
           let manager = new Manager(empName, empId, empEmail, mgrOffice);
           team.push(manager);
-          console.log(`Your employee is a(n) ${role}.`);
           break;
         case 'Engineer':
           let engineer = new Engineer(empName, empId, empEmail, github);
           team.push(engineer);
-          console.log(`Your employee is a ${role}.`);
           break;
         case 'Intern':
           let intern = new Intern(empName, empId, empEmail, internSchool);
           team.push(intern);
-          console.log(`Your employee is a ${role}.`);
           break;
         default:
           console.log('Your employee has no role.');
       }
-      return (team);
+      console.log(team)
+      createTeam(team)
     }
     )
-    .then((team) => {
-      fs.appendFile('./src/team.json', `${JSON.stringify(team, null, 2)}\n`, (err) => err ? console.error(err) : console.log('Team member created!'))
-    })
 }
-
+function createTeam(team) {
+  fs.appendFile('./src/team.json', `${JSON.stringify(team, null, 2)}\n`, (err) => err ? console.error(err) : console.log('Team member created!'))
+}
 
 // parseTeam = (team) => {
 //   const teamList = {}
