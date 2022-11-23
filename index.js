79f2cb7d144e400acb5c1ec076be750a6650a9f9
Mike Johnson <mike@mapleprintservices.com>
2022-11-22 22:54:45 -0500

    separated questions and success in new obj for single emp via prompt

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const questions = require('./lib/questions');
+const addMember = require('./lib/addMember');
+const teamQuest = require('./lib/teamQuest');
+
 // const employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const team = [];
 
-const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
-const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
-const Bob = new Intern('Bob', 456, 'bob@test.com', 'uconn')
+// const Mike = new Manager ('Mike', 45, 'mike@test.com', 46)
+// const Jackie = new Intern('Jackie', 123, 'jackie@test.com', 'RWU')
+// const Bob = new Intern('Bob', 456, 'bob@test.com', 'uconn')
 
-const Joe = new Engineer('Joe', 36, 'joe@test.com', 'bigJoeD');
-console.log(Mike);
-console.log(Joe);
-console.log(Jackie);
-console.log(Bob);
-function createTeam() {
-  return inquirer.prompt(questions.teamQuest)
+// const Joe = new Engineer('Joe', 36, 'joe@test.com', 'bigJoeD');
+// console.log(Mike);
+// console.log(Joe);
+// console.log(Jackie);
+// console.log(Bob);
+
+
+function inquireMember() {
+  return inquirer.prompt(addMember)
     .then((answers) => {
-      team.push(answers)
-      if (answers.addAnotherMember) {
-        return createTeam()
+      if (answers.addMember === 'Yes') {
+        createMember();
       } else {
-        console.log(team)
-        console.log(answers)
-        return (team)
+        console.log('Done!')
+        return
       }
+    })
+}
+
+function createMember() {
+  inquirer.prompt(teamQuest)
+    .then((answers) => {
+      let { empName, role, empId, empEmail, github, internSchool, mgrOffice, } = answers;
+      switch (role) {
+        case 'Manager':
+          let manager = new Manager(empName, empId, empEmail, mgrOffice);
+          team.push(manager);
+          console.log(`Your employee is a(n) ${role}.`);
+          break;
+        case 'Engineer':
+          let engineer = new Engineer(empName, empId, empEmail, github);
+          team.push(engineer);
+          console.log(`Your employee is a ${role}.`);
+          break;
+        case 'Intern':
+          let intern = new Intern(empName, empId, empEmail, internSchool);
+          team.push(intern);
+          console.log(`Your employee is a ${role}.`);
+          break;
+        default:
+          console.log('Your employee has no role.');
+      }
+      return (team);
     }
     )
     .then((team) => {
-      fs.appendFile('./src/team.json', `${JSON.stringify(team, null, '\t')}\n`, (err) => err ? console.error(err) : console.log('Team member created!'))
+      fs.appendFile('./src/team.json', `${JSON.stringify(team, null, 2)}\n`, (err) => err ? console.error(err) : console.log('Team member created!'))
     })
-  }
+}
 
-createTeam()
+
 // parseTeam = (team) => {
 //   const teamList = {}
 //   team.forEach(member => {
@@ -42,3 +71,4 @@ createTeam()
 //   })
 //   console.log(teamList)
 // }
+inquireMember()
